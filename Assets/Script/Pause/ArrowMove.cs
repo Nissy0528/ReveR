9a7824b72b79085cc4e85ab6fb6ce8f79fa43b02
AbyssQK <otakakimisato@icloud.com>
2017-05-19 16:32:48 +0900

    大高
    pause画面最低限完成

---
+++ Assets/Script/Pause/ArrowMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour {
+
+    // Use this for initialization
+    static bool IsSelect_3;
+    private static bool IsSelect_2;
+    private static bool IsSelect_1;
+
+    private Vector3 ArrowMoveVelocity;
+
+    public GameObject Select_1;
+    public GameObject Select_2;
+    public GameObject Select_3;
+    
+
+    void Start () {
+        
+        IsSelect_1 = false;
+        IsSelect_3 = false;
+        IsSelect_2 = false;
+        ArrowMoveVelocity = new Vector3(0, 56);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Select();
+        if (Input.GetKeyDown(KeyCode.DownArrow)&&IsSelect_3==false)
+            GetComponent<RectTransform>().position -= ArrowMoveVelocity;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)&&IsSelect_1==false)
+            GetComponent<RectTransform>().position += ArrowMoveVelocity;
+
+        Debug.Log(GetComponent<RectTransform>().position.y);
+    }
+    void Select()
+    {
+        if (Select_1.GetComponent<RectTransform>().position.y ==
+            GetComponent<RectTransform>().position.y)
+        {
+            IsSelect_1 = true;
+            IsSelect_2 = false;
+            IsSelect_3 = false;
+        }
+            
+
+        if (Select_2.GetComponent<RectTransform>().position.y == 
+            GetComponent<RectTransform>().position.y)
+        {
+            IsSelect_1 = false;
+            IsSelect_2 = true;
+            IsSelect_3 = false;
+        }
+
+        if (Select_3.GetComponent<RectTransform>().position.y == 
+            GetComponent<RectTransform>().position.y)
+        {
+            IsSelect_1 = false;
+            IsSelect_2 = false;
+            IsSelect_3 = true;
+        }
+    }
+    public bool GetIsSelect_1()
+    {
+        return IsSelect_1;
+    }
+    public bool GetIsSelect_2()
+    {
+        return IsSelect_2;
+    }
+    public bool GetIsSelect_3()
+    {
+        return IsSelect_3;
+    }
+}
---
+++ Assets/Script/Pause/Pause.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject PauseMenu;
+    private bool IsPause;
+    public GameObject arrowmove;
+	void Start () {
+        IsPause = false;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Pauseing();
+        
+        
+    }
+    void Pauseing()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (IsPause) { Pause_No(); }
+            else { Pause_Yes(); }
+        }
+        if (Input.GetKeyDown(KeyCode.J)&&IsPause)
+        {
+            if (arrowmove.GetComponent<ArrowMove>().GetIsSelect_1())
+            {
+                SceneManager.LoadScene("Title");
+                Pause_No();
+            }
+            if (arrowmove.GetComponent<ArrowMove>().GetIsSelect_2())
+            {
+                SceneManager.LoadScene("StageSelect");
+                Pause_No();
+            }
+            if (arrowmove.GetComponent<ArrowMove>().GetIsSelect_3())
+            {
+                SceneManager.LoadScene("Main");
+                Pause_No();
+            }
+        }
+    }
+    void Pause_Yes()
+    {
+        Time.timeScale = 0;
+        IsPause = true;
+        PauseMenu.SetActive(true);
+    }
+    void Pause_No()
+    {
+        Time.timeScale = 1;
+        IsPause = false;
+        PauseMenu.SetActive(false);
+    }
+
+
+}
