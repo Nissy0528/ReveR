75abccb4e8da8f97eb12694f8c75665fece9e686
AbyssQK <otakakimisato@icloud.com>
2017-05-22 11:07:25 +0900

    太高
    ポーズ画面完成

--- Assets/Script/Pause/Pause.cs
+++ Assets/Script/Pause/Pause.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,14 +9,28 @@ public class Pause : MonoBehaviour
 
     // Use this for initialization
     public GameObject PauseMenu;
-    private bool IsPause;
     public GameObject arrowmove;
+    
+    private bool IsPause;
+
+    private Behaviour[] Stop_Object_Behaviour;
+
+    [SerializeField]
+    public List<GameObject> Stop_Object;
     void Start()
     {
         IsPause = false;
 
-    }
+        Stop_Object.Add(GameObject.FindWithTag("Enemy"));
+        for (int i = 0; i < Stop_Object.Count; i++)
+        {
+            Stop_Object_Behaviour = Array.FindAll(Stop_Object[i].GetComponentsInChildren<Behaviour>(),
+           (obj) => { return obj.enabled; });
 
+
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
@@ -25,42 +40,55 @@ public class Pause : MonoBehaviour
     }
     void Pauseing()
     {
+        Pauseing_Meun();
+        Pauseing_MoveArrow();
+        
+    }
+    void Pauseing_Meun()
+    {
         if (Input.GetKeyDown(KeyCode.JoystickButton7))
         {
-            if (IsPause) { Pause_No(); }
-            else { Pause_Yes(); }
+            if (IsPause) { Get_Obj_Pause_No(); }
+            else { Get_Obj_Pause_Yes(); }
         }
+    }
+    void Pauseing_MoveArrow()
+    {
         if (Input.GetKeyDown(KeyCode.JoystickButton0) && IsPause)
         {
             if (arrowmove.GetComponent<ArrowMove>().GetIsSelect_1())
             {
                 SceneManager.LoadScene("Title");
-                Pause_No();
+                Get_Obj_Pause_No();
             }
             if (arrowmove.GetComponent<ArrowMove>().GetIsSelect_2())
             {
                 SceneManager.LoadScene("StageSelect");
-                Pause_No();
+                Get_Obj_Pause_No();
             }
             if (arrowmove.GetComponent<ArrowMove>().GetIsSelect_3())
             {
                 SceneManager.LoadScene("Main");
-                Pause_No();
+                Get_Obj_Pause_No();
             }
         }
     }
-    void Pause_Yes()
+    void Get_Obj_Pause_Yes()
     {
+        
+        foreach (var com in Stop_Object_Behaviour) com.enabled = false;
+
         Time.timeScale = 0;
         IsPause = true;
         PauseMenu.SetActive(true);
     }
-    void Pause_No()
+    void Get_Obj_Pause_No()
     {
+        foreach (var com in Stop_Object_Behaviour) com.enabled = true;
+
         Time.timeScale = 1;
         IsPause = false;
         PauseMenu.SetActive(false);
     }
-
 
 }
