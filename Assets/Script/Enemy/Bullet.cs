ff165f7862172b014b3f41178f4926e3d5a2455d
AbyssQK <otakakimisato@icloud.com>
2017-06-05 09:29:01 +0900

    大高
    itweenの代わり操作

---
+++ Assets/Script/Enemy/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public float speed;
+
+    private Vector3 Velocity;
+    private bool IsHit = false;
+
+    void Start()
+    {
+        Velocity = (GameObject.FindWithTag("Player").transform.position - transform.position).normalized;
+        Physics2D.IgnoreLayerCollision(9, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Velocity * speed;
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+
+
+
+}
