e17aa939d67a28fbbbeafebd8cc74f8cb27dcee7
Onuma0139 <syou018539@gmail.com>
2017-06-09 09:09:29 +0900

    星のアニメーション追加

---
+++ Assets/Script/Effect/StarAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarAnim : MonoBehaviour {
+
+    private Animator animator;
+    private int rnd;
+    private int count;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rnd = Random.Range(1, 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        count++;
+        if (count >= rnd)
+        {
+            animator.SetBool("Star", true);
+        }
+	}
+}
